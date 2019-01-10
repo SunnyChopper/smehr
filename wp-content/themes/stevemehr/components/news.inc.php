51abca446650b66953198d975d420b5ed4b29d2f
Sunny Singh <ishy.singh@gmail.com>
2019-01-10 00:44:58 -0600

    Debugging posts page

--- wp-content/themes/stevemehr/components/news.inc.php
+++ wp-content/themes/stevemehr/components/news.inc.php
@@ -1,3 +1,13 @@
+<div class="page-banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-12">
+				<h2 id="banner-text">News & Results</h2>
+			</div>
+		</div>
+	</div>
+</div>
+
 <?php
 	$args = array(
         'posts_per_page' => 4,
@@ -9,44 +19,38 @@
 	if ($featured->have_posts()): while($featured->have_posts()): $featured->the_post();
 ?>
 
-<div class="page-banner">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-				<h2 id="banner-text">News & Results</h2>
-			</div>
-		</div>
-	</div>
-</div>
-
 <div style="background-color: #eef0f1; padding: 24px;">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 col-md-8 col-sm-12 col-12">
 				<?php if($iterator_index == 0): ?>
-				<div class="row">
-					<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-						<a href="<?php the_permalink(); ?>">
-							<div class="main-story-box">
-								<div class="main-story-image">
-									<?php $thumb_id = get_post_thumbnail_id(); $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true); $thumb_url = $thumb_url_array[0]; ?>
-									<img src="<?php echo $thumb_url; ?>" class="regular-image">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12 col-12">
+							<a href="<?php the_permalink(); ?>">
+								<div class="main-story-box">
+									<div class="main-story-image">
+										<?php $thumb_id = get_post_thumbnail_id(); $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true); $thumb_url = $thumb_url_array[0]; ?>
+										<img src="<?php echo $thumb_url; ?>" class="regular-image">
+									</div>
+									<div class="main-story-info">
+										<h4><?php the_title(); ?></h4>
+									</div>
 								</div>
-								<div class="main-story-info">
-									<h4><?php the_title(); ?></h4>
-								</div>
-							</div>
-						</a>
+							</a>
+						</div>
 					</div>
-				</div>
+				<?php endif; ?>
 
+				<?php if(count($featured) > 1): ?>
+				<?php if($iterator_index == 1): ?>
 				<div class="row mt-4">
-					<?php else: ?>
+				<?php endif; ?>
+					<?php if($iterator_index > 0): ?>
 					<div class="col-lg-4 col-md-4 col-sm-4 col-12">
 						<div class="blog-box">
 							<a href="">
 								<div class="blog-box-image">
-									<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/blog-image-1.png" class="regular-image">
+									<img src="" class="regular-image">
 								</div>
 								<div class="blog-box-info">
 									<h5>Attorney Steven Mehr creates nation’s first law firm with former Judges and Senators on staff counsel</h5>
@@ -55,39 +59,16 @@
 						</div>
 					</div>
 					<?php endif; ?>
-
-					<!-- <div class="col-lg-4 col-md-4 col-sm-4 col-12">
-						<div class="blog-box">
-							<a href="">
-								<div class="blog-box-image">
-									<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/blog-image-1.png" class="regular-image">
-								</div>
-								<div class="blog-box-info">
-									<h5>Attorney Steven Mehr creates nation’s first law firm with former Judges and Senators on staff counsel</h5>
-								</div>
-							</a>
-						</div>
-					</div>
-
-					<div class="col-lg-4 col-md-4 col-sm-4 col-12">
-						<div class="blog-box">
-							<a href="">
-								<div class="blog-box-image">
-									<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/blog-image-1.png" class="regular-image">
-								</div>
-								<div class="blog-box-info">
-									<h5>Attorney Steven Mehr creates nation’s first law firm with former Judges and Senators on staff counsel</h5>
-								</div>
-							</a>
-						</div>
-					</div> -->
+				<?php endif; ?>
 				</div>
 			</div>
 
 			<?php
+				echo "Iterator index: " . $iterator_index . "\n";
 				$iterator_index += 1;
 				endwhile; else:
 				endif;
 			?>
-
-						
+		</div>
+	</div>
+</div>			
