14fa7c31535f17a381a9cb50368717fa1d32030c
Sunny Singh <ishy.singh@gmail.com>
2019-01-20 01:26:53 -0600

    ...

--- wp-content/themes/stevemehr/components/home.inc.php
+++ wp-content/themes/stevemehr/components/home.inc.php
@@ -424,16 +424,14 @@
 			</div>
 
 			<div class="row" style="display: flex;">
-				<div class="col-lg-6 col-md-6 col-sm-12 col-12" style="margin: auto;">
-					<p>This firm was founded based on the observation that only large corporations and the powerful could afford top legal representation while the rest of us were left without fair access. In response, I built this law firm to level the playing field and protect everyday people. When you hire us, you are hiring more than just lawyers. You are hiring a team of retired prosecutors, judges and educators who have dedicated their entire lives fighting for injustice and to help everyday people.</p>
-					<p>When you suffer a personal injury or lose a loved one in an accident, it can be life changing and devastating. Our compassionate lawyers have seen some of the worst human tragedies, so we realize the power we have to help people in their greatest time of need. Our number one job is to take care of you and put you in a better place. Every single day we fight battles for our clients’ rights – and deliver justice for our clients and their families when they are most vulnerable. Let us help you get to a better place.</p>
-				</div>
-
-				<div class="col-lg-5 col-md-6 col-sm-12 col-12" style="margin: auto;">
+				<div class="col-lg-5 col-md-12 col-sm-12 col-12" style="margin: auto;">
 					<img src="http://rich20something.com/wp-content/uploads/2015/10/maxresdefault-5.jpg" class="regular-image">
 				</div>
 
-				
+				<div class="col-lg-7 col-md-12 col-sm-12 col-12 mt-32-tablet mt-32-mobile" style="margin: auto;">
+					<p>This firm was founded based on the observation that only large corporations and the powerful could afford top legal representation while the rest of us were left without fair access. In response, I built this law firm to level the playing field and protect everyday people. When you hire us, you are hiring more than just lawyers. You are hiring a team of retired prosecutors, judges and educators who have dedicated their entire lives fighting for injustice and to help everyday people.</p>
+					<p>When you suffer a personal injury or lose a loved one in an accident, it can be life changing and devastating. Our compassionate lawyers have seen some of the worst human tragedies, so we realize the power we have to help people in their greatest time of need. Our number one job is to take care of you and put you in a better place. Every single day we fight battles for our clients’ rights – and deliver justice for our clients and their families when they are most vulnerable. Let us help you get to a better place.</p>
+				</div>
 			</div>
 		</div>
 	</section>
--- wp-content/themes/stevemehr/components/main-banner.inc.php
+++ wp-content/themes/stevemehr/components/main-banner.inc.php
@@ -1,7 +1,7 @@
 <div class="main-banner">
 	<div class="container" id="banner-padded-area">
 		<div class="row">
-			<div class="col-lg-6 offset-lg-6 col-md-6 offset-md-6 col-sm-8 offset-sm-4 col-8 offset-4">
+			<div class="col-lg-6 offset-lg-6 col-md-6 offset-md-6 col-sm-8 offset-sm-4 col-9 offset-3">
 				<h3 id="main-banner-tagline">Steve is the best consumer advocate in the country.</h3>
 				<div class="row">
 					<div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
