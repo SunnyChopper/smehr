17ba408229770fa2e1f1e850a25e355d237a5c08
Sunny Singh <ishy.singh@gmail.com>
2019-01-19 22:14:05 -0600

    ...

--- wp-content/themes/stevemehr/components/main-banner.inc.php
+++ wp-content/themes/stevemehr/components/main-banner.inc.php
@@ -2,35 +2,8 @@
 	<div class="container" id="banner-padded-area">
 		<div class="row">
 			<div class="col-lg-6 col-md-6 col-sm-12 col-12">
-<!-- 				<h1 class="text-center d-md-none" id="main-banner-title"><b>Over $1 Billion<br>won for our clients</b></h1>
-
-				<section version="mobile-version" class="d-md-none">
-					<div class="row mb-0" id="first-support-text">
-						<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-							<p class="banner-support-text"><span style="float: left; margin-left: 16px;"><i class="fa fa-check" style="color: #00c5ab;"></i></span> Retired Judges of Counsel</p>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-							<p class="banner-support-text"><span style="float: left; margin-left: 16px;"><i class="fa fa-check" style="color: #00c5ab;"></i></span> Retired Senators of Counsel</p>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-							<p class="banner-support-text"><span style="float: left; margin-left: 16px;"><i class="fa fa-check" style="color: #00c5ab;"></i></span> No fees unless we win</p>
-						</div>
-					</div>
-
-					<div class="row mb-4">
-						<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-							<p class="banner-support-text"><span style="float: left; margin-left: 16px;"><i class="fa fa-check" style="color: #00c5ab;"></i></span> 98.7% Win Rate</p>
-						</div>
-					</div>
-				</section>
-			</div> -->
-
+			</div>
+			
 			<div class="col-lg-6 col-md-6 col-sm-12 col-12">
 				<h3 id="main-banner-tagline">Steve is the best consumer advocate in the country.</h3>
 				<div class="row">
