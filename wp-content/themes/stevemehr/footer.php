6d54f12d165686a30a03a99a90e00d4c59a5e6da
Sunny Singh <ishy.singh@gmail.com>
2019-01-25 15:21:03 -0600

    ...

--- wp-content/themes/stevemehr/footer.php
+++ wp-content/themes/stevemehr/footer.php
@@ -9,7 +9,7 @@
 			<div class="col-lg-8 col-md-8 col-sm-12 col-12">
 				<div class="row">
 					<div class="col-12">
-						<p class="text-center mb-2"><b>Nationwide Help | No Fees Until We Win | 24/7 Attorney Access | 100% Free Consultations</b></p>
+						<p class="text-center mb-4"><b>Nationwide Help | No Fees Until We Win | 24/7 Attorney Access | 100% Free Consultations</b></p>
 					</div>
 
 					<div class="col-lg-3 col-md-3 col-sm-6 col-6">
