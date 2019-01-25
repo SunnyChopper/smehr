f92b07c81bb9793f056cdff877e1f7ea1b53b92b
Sunny Singh <ishy.singh@gmail.com>
2019-01-25 13:14:35 -0600

    ...

--- wp-content/themes/stevemehr/components/main-banner.inc.php
+++ wp-content/themes/stevemehr/components/main-banner.inc.php
@@ -4,7 +4,7 @@
 			<div class="col-lg-6 offset-lg-6 col-md-6 offset-md-6 col-sm-8 offset-sm-4 col-9 offset-3">
 				<h3 id="main-banner-tagline">Steve is the best consumer advocate in the country.</h3>
 				<div class="row">
-					<div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
+					<div class="col-12">
 						<h5 id="tagline-person" class="text-white">- Wayne Gross</h5>
 						<p id="tagline-ref" class="text-white"><small><i>Retired Chief of the U.S, Attorneys Office</i></small></p>
 					</div>
