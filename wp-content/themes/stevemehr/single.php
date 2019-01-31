0450f8f37a7c84eb6ca18587b83d5b2a3950ab01
Sunny Singh <ishy.singh@gmail.com>
2019-01-31 17:51:24 -0600

    ...

--- wp-content/themes/stevemehr/single.php
+++ wp-content/themes/stevemehr/single.php
@@ -1,22 +1,30 @@
 <?php get_header(); ?>
 <?php require_once('components/navbar.inc.php'); ?>
 
-<div class="container">
+<div class="container" id="post-container">
 	<div class="row">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<?php if (has_post_thumbnail()) : ?>
-				<?php the_post_thumbnail(); ?>
+		<div class="col-lg-8 col-md-8 col-sm-12 col-12">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php if (has_post_thumbnail()) : ?>
+					<?php the_post_thumbnail(); ?>
+				<?php endif; ?>
+
+				<h3><?php the_title(); ?></h3>
+				<hr />
+				<?php the_content(); ?>
+				<hr />
+				<?php echo get_the_date(); ?>
+
+			<?php endwhile; ?>
 			<?php endif; ?>
-
-			<h3><?php the_title(); ?></h3>
-			<?php the_content(); ?>
-			<?php echo get_the_date(); ?>
-
-		<?php endwhile; ?>
-		<?php endif; ?>
+		</div>
 	</div>
 </div>
 
-
+<style type="text/css">
+	#post-container {
+		margin-top: 128px;
+	}
+</style>
 
 <?php get_footer(); ?>
