f254b463856fdfc5b1fffe3b0fbfdc2860eb3cf9
Sunny Singh <ishy.singh@gmail.com>
2019-01-31 17:48:50 -0600

    ...

--- wp-content/themes/stevemehr/single.php
+++ wp-content/themes/stevemehr/single.php
@@ -1,13 +1,22 @@
 <?php get_header(); ?>
 <?php require_once('components/navbar.inc.php'); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="container">
+	<div class="row">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php if (has_post_thumbnail()) : ?>
+				<?php the_post_thumbnail(); ?>
+			<?php endif; ?>
 
-<?php the_title(); ?>
-<?php the_content(); ?>
-<?php echo get_the_date(); ?>
+			<h3><?php the_title(); ?></h3>
+			<?php the_content(); ?>
+			<?php echo get_the_date(); ?>
 
-<?php endwhile; ?>
-<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</div>
+
+
 
 <?php get_footer(); ?>
