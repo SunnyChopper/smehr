8feb383fede3d5bd83267d57d365bce0de875361
Sunny Singh <ishy.singh@gmail.com>
2019-02-01 15:59:38 -0600

    ...

--- wp-content/themes/stevemehr/index.php
+++ wp-content/themes/stevemehr/index.php
@@ -4,19 +4,19 @@
 	global $wp;
 	$slug = $wp->request;
 	if (is_front_page()) {
-		require_once('components/main-banner.inc.php'); 
+		require('components/main-banner.inc.php'); 
 		the_content();
-		require_once('components/firm-info.inc.php');
-		require_once('components/how-we-help.inc.php');
-		require_once('components/results.inc.php');
-		require_once('components/client-stories.inc.php');
-		require_once('components/features.inc.php');
-		require_once('components/steve-bio.inc.php');
-		require_once('components/bottom-cta.inc.php'); 
+		require('components/firm-info.inc.php');
+		require('components/how-we-help.inc.php');
+		require('components/results.inc.php');
+		require('components/client-stories.inc.php');
+		require('components/features.inc.php');
+		require('components/steve-bio.inc.php');
+		require('components/bottom-cta.inc.php'); 
 	} elseif (is_home()) {
-		require_once('components/news.inc.php');
-		require_once('components/client-stories.inc.php');
-		require_once('components/bottom-cta.inc.php'); 
+		require('components/news.inc.php');
+		require('components/client-stories.inc.php');
+		require('components/bottom-cta.inc.php'); 
 	} else {
 		if (have_posts()):
 		  while (have_posts()) : the_post();
