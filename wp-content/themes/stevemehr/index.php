d9fb6eeac4f4a84175aad9666579d552dad18170
Sunny Singh <ishy.singh@gmail.com>
2019-02-01 15:58:19 -0600

    ...

--- wp-content/themes/stevemehr/index.php
+++ wp-content/themes/stevemehr/index.php
@@ -2,13 +2,21 @@
 <?php require_once('components/navbar.inc.php'); ?>
 <?php 
 	global $wp;
-	echo $wp->request;
+	$slug = $wp->request;
 	if (is_front_page()) {
 		require_once('components/main-banner.inc.php'); 
 		the_content();
+		require_once('components/firm-info.inc.php');
+		require_once('components/how-we-help.inc.php');
+		require_once('components/results.inc.php');
+		require_once('components/client-stories.inc.php');
+		require_once('components/features.inc.php');
+		require_once('components/steve-bio.inc.php');
 		require_once('components/bottom-cta.inc.php'); 
 	} elseif (is_home()) {
 		require_once('components/news.inc.php');
+		require_once('components/client-stories.inc.php');
+		require_once('components/bottom-cta.inc.php'); 
 	} else {
 		if (have_posts()):
 		  while (have_posts()) : the_post();
