1cd27cf1a38899d056881f47caf303553cdccc36
Sunny Singh <ishy.singh@gmail.com>
2019-02-01 16:02:16 -0600

    ...

---
+++ wp-content/themes/stevemehr/components/client-stories.inc.php
@@ -0,0 +1,111 @@
+<div id="clients-row">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-12">
+				<h4 style="margin-bottom: 24px; font-weight: 700; font-family: 'Roboto', sans-serif;" id="client-headline">Client Success Stories</h4>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-3 col-md-3 col-sm-6 col-6 low-padding-left-mobile low-padding-right-mobile">
+				<div class="video-box">
+					<div class="video-image">
+						<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/client-1.png" class="regular-image">
+						<div class="video-bar">
+							<div class="row justify-content-center">
+								<div class="col-lg-6 col-md-6 col-sm-8 col-10">
+									<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/stars.png" class="regular-image">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="video-info">
+						<div class="row">
+							<div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-10 offset-1">
+								
+							</div>
+						</div>
+
+						<p class="text-center name"><b>Roberta R. Roberts</b></p>
+						<h5 class="text-center quote">"Steve Mehr changed my life for the better. Steve Mehr changed my life for the better. Steve Mehr changed my life for the better."</h5>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-3 col-sm-6 col-6 low-padding-left-mobile low-padding-right-mobile">
+				<div class="video-box">
+					<div class="video-image">
+						<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/client-1.png" class="regular-image">
+						<div class="video-bar">
+							<div class="row justify-content-center">
+								<div class="col-lg-6 col-md-6 col-sm-8 col-10">
+									<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/stars.png" class="regular-image">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="video-info">
+						<div class="row">
+							<div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-10 offset-1">
+								
+							</div>
+						</div>
+
+						<p class="text-center name"><b>Roberta R. Roberts</b></p>
+						<h5 class="text-center quote">"Steve Mehr changed my life for the better. Steve Mehr changed my life for the better. Steve Mehr changed my life for the better."</h5>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-3 col-sm-6 col-6 low-padding-left-mobile low-padding-right-mobile">
+				<div class="video-box">
+					<div class="video-image">
+						<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/client-1.png" class="regular-image">
+						<div class="video-bar">
+							<div class="row justify-content-center">
+								<div class="col-lg-6 col-md-6 col-sm-8 col-10">
+									<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/stars.png" class="regular-image">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="video-info">
+						<div class="row">
+							<div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-10 offset-1">
+								
+							</div>
+						</div>
+
+						<p class="text-center name"><b>Roberta R. Roberts</b></p>
+						<h5 class="text-center quote">"Steve Mehr changed my life for the better. Steve Mehr changed my life for the better. Steve Mehr changed my life for the better."</h5>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-3 col-sm-6 col-6 low-padding-left-mobile low-padding-right-mobile">
+				<div class="video-box">
+					<div class="video-image">
+						<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/client-1.png" class="regular-image">
+						<div class="video-bar">
+							<div class="row justify-content-center">
+								<div class="col-lg-6 col-md-6 col-sm-8 col-10">
+									<img src="http://ec2-18-234-73-162.compute-1.amazonaws.com/wp-content/themes/stevemehr/images/stars.png" class="regular-image">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="video-info">
+						<div class="row">
+							<div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-10 offset-1">
+								
+							</div>
+						</div>
+
+						<p class="text-center name"><b>Roberta R. Roberts</b></p>
+						<h5 class="text-center quote">"Steve Mehr changed my life for the better. Steve Mehr changed my life for the better. Steve Mehr changed my life for the better."</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- wp-content/themes/stevemehr/index.php
+++ wp-content/themes/stevemehr/index.php
@@ -3,20 +3,21 @@
 <?php 
 	global $wp;
 	$slug = $wp->request;
+	
 	if (is_front_page()) {
-		require('components/main-banner.inc.php'); 
+		include('components/main-banner.inc.php'); 
 		the_content();
-		require('components/firm-info.inc.php');
-		require('components/how-we-help.inc.php');
-		require('components/results.inc.php');
-		require('components/client-stories.inc.php');
-		require('components/features.inc.php');
-		require('components/steve-bio.inc.php');
-		require('components/bottom-cta.inc.php'); 
+		include('components/firm-info.inc.php');
+		include('components/how-we-help.inc.php');
+		include('components/results.inc.php');
+		include('components/client-stories.inc.php');
+		include('components/features.inc.php');
+		include('components/steve-bio.inc.php');
+		include('components/bottom-cta.inc.php'); 
 	} elseif (is_home()) {
-		require('components/news.inc.php');
-		require('components/client-stories.inc.php');
-		require('components/bottom-cta.inc.php'); 
+		include('components/news.inc.php');
+		include('components/client-stories.inc.php');
+		include('components/bottom-cta.inc.php'); 
 	} else {
 		if (have_posts()):
 		  while (have_posts()) : the_post();
