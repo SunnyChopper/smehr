5f4e30c0981ed8be2053cd4ab694a2cebdabbc26
Sunny Singh <ishy.singh@gmail.com>
2019-02-06 16:13:45 -0600

    Attempt to create template

--- wp-content/themes/stevemehr/practice-area-single.php
+++ wp-content/themes/stevemehr/practice-area-single.php
@@ -8,11 +8,9 @@
 get_header();
 ?>
 
-<div class="container">
-	<div class="col-12">
 		<?php the_content(); ?>
-	</div>
-</div>
+
+<?php 
 
 include('components/results.inc.php');
 include('components/client-stories.inc.php');
