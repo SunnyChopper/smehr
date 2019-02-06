7cebf95d87c07170e035967b107692cd4355e8fc
Sunny Singh <ishy.singh@gmail.com>
2019-02-06 11:11:08 -0600

    ...

--- wp-content/themes/stevemehr/index.php
+++ wp-content/themes/stevemehr/index.php
@@ -9,10 +9,10 @@
 		include('components/main-banner.inc.php'); 
 		the_content();
 		include('components/firm-info.inc.php');
-		include('components/how-we-help.inc.php');
+		include('components/features.inc.php');
 		include('components/results.inc.php');
 		include('components/client-stories.inc.php');
-		include('components/features.inc.php');
+		include('components/how-we-help.inc.php');
 		include('components/steve-bio.inc.php');
 		include('components/bottom-cta.inc.php'); 
 	} elseif (is_home()) {
