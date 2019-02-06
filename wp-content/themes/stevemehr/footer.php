f020edd3b83e9f9d36eceabb765779bdb2b3586f
Sunny Singh <ishy.singh@gmail.com>
2019-02-06 17:49:17 -0600

    Footer styles

--- wp-content/themes/stevemehr/footer.php
+++ wp-content/themes/stevemehr/footer.php
@@ -39,7 +39,7 @@
 			<div class="col-lg-12 col-md-12 col-sm-12 col-12">
 				<p id="copyright-text">&copy; 2019 Steve Mehr | All Rights Reserved.</p>
 
-				<p class="footer-link text-center mb-0">Lawyer Advertising.</p>
+				<p class="text-center mb-0">Lawyer Advertising.</p>
 				<?php 
 					wp_nav_menu(
 						array( 
