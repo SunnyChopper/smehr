3e66ac74617d831de4ab4b9ec8aa1b4986fe5e71
Sunny Singh <ishy.singh@gmail.com>
2019-02-05 23:35:08 -0600

    ...

--- wp-content/themes/stevemehr/components/navbar.inc.php
+++ wp-content/themes/stevemehr/components/navbar.inc.php
@@ -17,21 +17,22 @@
 				<span class="navbar-toggler-icon"></span>
 			</button>
 
-			<?php
-				wp_nav_menu(
-					array( 
-					    'theme_location' => 'main-menu', 
-					    'container' => false,
-					    'depth' => 2,
-					    'container'       => 'div',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id' => 'bs-example-navbar-collapse-1',
-						'menu_class' => 'nav',
-						'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-						'walker' => new WP_Bootstrap_Navwalker()
-					) 
-				);
-			?>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<?php
+						wp_nav_menu(
+							array( 
+							    'theme_location' => 'main-menu', 
+							    'container' => false,
+							    'depth' => 2,
+								'menu_class' => 'nav',
+								'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+								'walker' => new WP_Bootstrap_Navwalker()
+							) 
+						);
+					?>
+				</ul>
+			</div>
 		</div>	
 	</nav>
 </section>
