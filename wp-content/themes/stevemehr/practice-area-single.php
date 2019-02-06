2c473a14fb502b0bd4913bf62b908e68cf41b808
Sunny Singh <ishy.singh@gmail.com>
2019-02-06 16:12:42 -0600

    Attempt to create template

--- wp-content/themes/stevemehr/practice-area-single.php
+++ wp-content/themes/stevemehr/practice-area-single.php
@@ -5,13 +5,21 @@
   Page template for practice area
  */
 
-get_header(); 
-the_content();
+get_header();
+?>
+
+<div class="container">
+	<div class="col-12">
+		<?php the_content(); ?>
+	</div>
+</div>
+
 include('components/results.inc.php');
 include('components/client-stories.inc.php');
 include('components/features.inc.php');
 include('components/attorneys.inc.php');
 include('components/bottom-cta.inc.php');
+get_footer();
 
 ?>
 
