627fde7fb280d3fd5b322037062919dc5f4b850d
Sunny Singh <ishy.singh@gmail.com>
2019-02-18 04:22:04 -0600

    ...

--- wp-content/themes/stevemehr/single.php
+++ wp-content/themes/stevemehr/single.php
@@ -5,14 +5,10 @@
 	<div class="row">
 		<div class="col-lg-8 col-md-8 col-sm-12 col-12">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				<?php if (has_post_thumbnail()) : ?>
-					<?php the_post_thumbnail('large', array('class' => 'regular-image')); ?>
-				<?php endif; ?>
-
 				<h3 class="mt-32"><?php the_title(); ?></h3>
 				<hr />
-					<?php the_content(); ?>
-
+				<?php the_content(); ?>
+				<!--more-->
 			<?php endwhile; ?>
 			<?php endif; ?>
 		</div>
