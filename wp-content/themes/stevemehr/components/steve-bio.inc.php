98321f9a710a4444cfced750c8fcc639fc34630f
Sunny Singh <ishy.singh@gmail.com>
2019-02-18 03:51:26 -0600

    Updated bio section

--- wp-content/themes/stevemehr/components/steve-bio.inc.php
+++ wp-content/themes/stevemehr/components/steve-bio.inc.php
@@ -1,18 +1,13 @@
 <div style="background-color: #eef0f1; padding: 24px;">
 	<section version="desktop-version">
 		<div class="container">
-			<div class="row" style="margin-bottom: 24px;">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-					<h3 style="font-family: 'Roboto', sans-serif;"><b>A Little Bit About Steve Mehr Injury Lawyers</b></h3>
-				</div>
-			</div>
-
 			<div class="row">
-				<div class="col-lg-5 col-md-12 col-sm-12 col-12">
+				<div class="col-lg-6 col-md-5 col-sm-12 col-12">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/steve.jpg" class="regular-image">
 				</div>
 
-				<div class="col-lg-7 col-md-12 col-sm-12 col-12 mt-32-tablet mt-32-mobile">
+				<div class="col-lg-6 col-md-7 col-sm-12 col-12 mt-32-tablet mt-32-mobile" style="background-color: rgba(66, 99, 155, 1); padding: 24px;">
+					<h3 style="font-family: 'Roboto', sans-serif;"><b>A Little Bit About Steve Mehr Injury Lawyers</b></h3>
 					<p>This firm was founded based on the observation that only large corporations and the powerful could afford top legal representation while the rest of us were left without fair access. In response, I built this law firm to level the playing field and protect everyday people. When you hire us, you are hiring more than just lawyers. You are hiring a team of retired prosecutors, judges and educators who have dedicated their entire lives fighting for injustice and to help everyday people.</p>
 					<p>When you suffer a personal injury or lose a loved one in an accident, it can be life changing and devastating. Our compassionate lawyers have seen some of the worst human tragedies, so we realize the power we have to help people in their greatest time of need. Our number one job is to take care of you and put you in a better place. Every single day we fight battles for our clients’ rights – and deliver justice for our clients and their families when they are most vulnerable. Let us help you get to a better place.</p>
 				</div>
